d8ad39f5e7a6b77e64dba895c13446e2991aa842
Jeremy Tucker <jeremytucker76@gmail.com>
2022-08-12 12:56:14 -0400

    variable exercise

---
+++ variableAssignment/Program.cs
@@ -0,0 +1,7 @@
+﻿int userAge = 37;
+string userName = "Jeremy";
+string userSchool = "True Coder's Academy";
+char userScore = 'A';
+
+
+Console.WriteLine($"Hi My name is {userName}, I am {userAge} and i attend {userSchool}, and this assignment got me an {userScore}.");
